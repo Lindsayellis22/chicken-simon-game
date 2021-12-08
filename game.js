e1755692bdd51d265d5f21b9dda73f13e614ab7b
lindsayellis22 <lindsayellis39@gmail.com>
2021-12-08 15:52:03 -0500

    add more js logic

--- game.js
+++ game.js
@@ -1,34 +1,53 @@
 console.log('hi');
 // const chickenMovesArray = [];
 const playerMovesArray = [];
-// let numberOfRounds = 10;
-const startButton = document.querySelector('button');
-
+let numberOfRounds = 0;
+const startButton = document.getElementById('start');
+console.log(startButton);
 
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-    console.log('I was clicked');
+    // chickenShow();
+    if (numberOfRounds === 0) {
+        chickenShow(); 
+    }
 }
 
 
 
-function gameRounds() {
-    let numberOfRounds = 10;
-    const chickenMovesArray = [0, 1, 2, 3];
-    randomChicken = chickenMovesArray[Math.floor(Math.random()*chickenMovesArray.length)];
-    console.log(randomChicken)
-    // chickenMovesArray.push("");
-}
-gameRounds();
+// function gameRounds() {
+//     let numberOfRounds = 0;
+//     const chickenMovesArray = [0, 1, 2, 3];
+//     randomChicken = chickenMovesArray[Math.floor(Math.random()*chickenMovesArray.length)];
+//     console.log(randomChicken)
+//     // chickenMovesArray.push("");
+// }
+// gameRounds();
 
 const chickenSprite = document.getElementById('0');
 console.log(chickenSprite);
-chickenSprite.addEventListener("click", clickChicken);
+chickenSprite.addEventListener("click", () => changeColor(0));
 
-function clickChicken() {
+
+
+function changeColor(chickenNumber) {
     
-    console.log('hello chicken')
+    const chickenBody = document.getElementById(`body${chickenNumber}`);
+    chickenBody.style.background = "red";
+    
 }
+function chickenShow() {
+    const arr = [0, 2, 1, 3];
+    arr.map(chickenNum => {
+        changeColor(chickenNum);
+    }, 2000);
+}
+
+// chickenShow();
+
+
+
+
 
 
