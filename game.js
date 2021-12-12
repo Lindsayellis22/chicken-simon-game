453f4bb2cfbb96bf39573ce65be29dd311dda3af
lindsayellis22 <lindsayellis39@gmail.com>
2021-12-12 13:08:52 -0500

    fixed chicken change color bug

--- game.js
+++ game.js
@@ -1,94 +1,69 @@
+let playerMovesArray = [];
 let chickenMovesArray = [];
 let round = 0;
-const chick0 = document.getElementById('0');
-const chick1 = document.getElementById('1');
-const chick2 = document.getElementById('2');
-const chick3 = document.getElementById('3');
+
+document.getElementById('0').addEventListener('click', chickClickHandler);
+document.getElementById('1').addEventListener('click', chickClickHandler);
+document.getElementById('2').addEventListener('click', chickClickHandler);
+document.getElementById('3').addEventListener('click', chickClickHandler);
 
 const startButton = document.getElementById('start');
 
-startButton.addEventListener("click", colorChickens);
+startButton.addEventListener("click", newGame);
+
+function newGame() {
+    chickenMovesArray = [];
+    playerMovesArray = [];
+    round = 0;
+    colorChickens();
+};
 
 function colorChickens() {
-    round += 1;
-    // 1. push a random chicken into the chickenMoves array.
     chickenMovesArray.push(Math.floor(Math.random() * 3));
     chickenMovesArray.map((chicken, i) => {
         setTimeout(() => {
-        // 2. loop through chcieknMoves Array and for each chicken, make chicken color for 1 second
-            const currentChicken = document.getElementById(`body${chicken}`);
+            const currentChicken = document.getElementById(`${chicken}`);
             setTimeout(()=> makeChickenPink(currentChicken), 1000);
             setTimeout(()=> makeChickenBisque(currentChicken), 2000);
         }, i * 2000);
     });
-    setTimeout(playerClick, (chickenMovesArray.length) * 2000);
 }
 
-// function gameRound() {
-//     console.log("nextround");
-//     playerMovesArray.splice(0, playerMovesArray.length);
-//     return round += 1; 
-// };
-
 function makeChickenBisque(currentChicken) {
-    currentChicken.style.background = "bisque";
+    currentChicken.querySelector('.head').style.background = "bisque";
+    currentChicken.querySelector('.tail').style.background = "bisque";
+    currentChicken.querySelector('.body').style.background = "bisque";
 }
 
 function makeChickenPink(currentChicken) {
-    currentChicken.style.background = "hotpink";
+    currentChicken.querySelector('.head').style.background = "#f19cbb";
+    currentChicken.querySelector('.tail').style.background = "#f19cbb";
+    currentChicken.querySelector('.body').style.background = "#f19cbb";
 }
 
-function chickClickHandler(id, playerMovesArray) {
-    console.log('clickedChick');
-    playerMovesArray.push(id);
-}
-
-function listenForPlayer(playerMovesArray) {
-    console.log('player clicked')
-    if (playerMovesArray.length === chickenMovesArray.length){
-        checkIfWinner(playerMovesArray);
-    } 
-}
-
-function playerClick() {
-    let playerMovesArray = new Array();
-    document.getElementById('chickenContainer').addEventListener('click', () => listenForPlayer(playerMovesArray));
-    console.log('inside player click');
-    chick0.addEventListener("click", ()  => chickClickHandler(0, playerMovesArray));
-    chick1.addEventListener("click", ()  => chickClickHandler(1, playerMovesArray));
-    chick2.addEventListener("click", ()  => chickClickHandler(2, playerMovesArray));
-    chick3.addEventListener("click", ()  => chickClickHandler(3, playerMovesArray));
-        
-    
-    console.log(chickenMovesArray, 'cma');
-}
-
-function clearEventListeners() {
-    console.log('clear event listeners')
-    document.getElementById('chickenContainer').removeEventListener('click', listenForPlayer);
-    chick0.removeEventListener('click', chickClickHandler);
-    chick1.removeEventListener('click', chickClickHandler);
-    chick2.removeEventListener('click', chickClickHandler);
-    chick3.removeEventListener('click', chickClickHandler);
-    console.log(chick0);
+function nextRound() {
+    console.log('next round')
+    round += 1;
+    playerMovesArray = [];
+    colorChickens();
 };
 
-function checkIfWinner(arr) {
-    clearEventListeners();
-    console.log(arr, "pma inside check winner and your mom");
-    console.log('inside checkwinner');
-    if(arr.length !== chickenMovesArray.length) {
-        console.log('you are posessed')
-        return false;
+function chickClickHandler(event) {
+    console.log(event);
+    playerMovesArray.push(parseInt(event.target.dataset.value));
+    if (playerMovesArray.length === chickenMovesArray.length){
+        checkIfWinner();
     }
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] !== chickenMovesArray[i]) {      
+}
+
+function checkIfWinner() {
+    for (let i = 0; i < playerMovesArray.length; i++) {
+        if (playerMovesArray[i] !== chickenMovesArray[i]) {      
             console.log('false and lost');
-            return false;
+            return "lost";
         }
     }
-    colorChickens();
-    return true;
+    nextRound();
 }
             
 // let testButton = document.getElementById('test');
